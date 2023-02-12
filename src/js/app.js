6d573eb2d0c1a79287c00a71da0950d2e3fda4c0
viktor0980 <viktor0980@gmail.com>
2023-02-12 20:10:36 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,21 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+import "../scss/app.scss";
+
+window.addEventListener("DOMContentLoaded", () => {
+  // This block will be executed once the page is loaded and ready
+ function image() { const collection = document.querySelector(".main");
+const img = collection.document.createElement("div");
+img.classlist.add("image");
+document.body.insertBefore(img, collection); }
+image();
+
+  const button = document.querySelector(".button");
+  button.addEventListener("click", () => {
+    alert("💣");
+  });
+});
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
