9d28e66581e0f9924b17ac1e803892f62e96c07f
viktor0980 <viktor0980@gmail.com>
2023-02-12 21:45:14 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,15 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-const img = document.createElement("div");
+var img = document.createElement("div");
 img.classList.add("image");
 
-const main = document.getElementsByClassName("main");
+var main = document.getElementsByClassName("main");
 main[0].appendChild(img);
-
-const image = document.createElement("img");
+var image = document.createElement("img");
 image[0].setAttribute("src", "image");
-const image2 = document.getElementsByClassName("image");
+image.src = "https://www.shutterstock.com/image-vector/man-icon-vector-260nw-1040084344.jpg"
+var image2 = document.getElementsByClassName("image");
 main[0].appendChild(image2);
 image.resize(200, 200);
 
